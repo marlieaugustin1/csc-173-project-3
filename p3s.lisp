2387e49b63a2ea5e008b0e82a7885e27d4c6f3fb
Jaddy1 <jeffronaddy@gmail.com>
2019-04-01 22:34:20 -0400

    Right triangle function

--- p3s.lisp
+++ p3s.lisp
@@ -10,18 +10,28 @@
 
 (defun addToEnd (x list)
   (reverseList (cons x (reverse list)))
-)
-(print "Function to an element to the end of a list")
+)		
+
+(defun rightTriangle (x y z) 
+ (if (or (eq (+(* x x) (* y y)) (* z z) ) (eq (+(* x x) (* z z)) (* y y) ) 
+ 	(eq (+(* y y) (* z z)) (* x x) ) )
+  	(print "Valid right Triangle")
+  	nil 
+	))
+(print "Check if 3 integers can be the lengths of the two sides and the hypotenuse of a right triangle (in that order)")
 (terpri)
-(print "Type in a list you want the element to be added to")
+(print "Input one side")
 (terpri)
-(setq list (read))
+(setq s1 (read))
+(print "Input the second side")
 (terpri)
-(print "Enter the element")
+(setq s2 (read))
+(print "Input the hypotenuse")
 (terpri)
-(setq el (read))
+(setq h (read))
+(print (rightTriangle s1 s2 h))
 (terpri)
-(print (addToEnd el list))		
+
 
 
 (defun test (init)
